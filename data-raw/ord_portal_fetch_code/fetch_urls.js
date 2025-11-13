82ca6ed05c5ddd9cf5db1b5849cc85983c57b082
Nicolo Massari <nmassari@ethz.ch>
2025-11-13 16:17:29 +0100

    chore: clean previous ethord

--- data-raw/ord_portal_fetch_code/fetch_urls.js
+++
@@ -1,8 +0,0 @@
-const dataUrls = Array.from(document.querySelectorAll('[data-url]'))
-  .map(element => element.getAttribute('data-url'));
-
-console.log(dataUrls);
-
-// To copy the array to your clipboard:
-copy(dataUrls.join('\n'));
-console.log('Data URLs copied to clipboard!');
